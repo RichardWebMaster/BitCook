45524928c9353a855c689e61a419389701791138
Mi nombre y apellido <nombre@gmail.com>
2024-08-12 15:39:56 -0500

    Cargando archivos

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function activarModoOscuro() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white"; // Cambia el color del texto en modo oscuro
+}
+
+function activarModoClaro() {
+    document.body.style.backgroundColor = "#F2E4D8"; // Color de fondo claro
+    document.body.style.color = "black"; // Cambia el color del texto en modo claro
+}
